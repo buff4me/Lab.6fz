eed95de3470ce908277579d6ccbe678e274be98e
Faizan Ahmad <faizan.ptk303@gmail.com>
2025-02-27 11:05:21 +0500

    Update Grocerylist.cs

--- Grocerylist.cs
+++ Grocerylist.cs
@@ -6,6 +6,7 @@ class Program
     static void Main()
     {
         // List of grocery items
+        groceryItems.Add("Oranges");
         List<string> groceryItems = new List<string>
         {
             "Milk",
